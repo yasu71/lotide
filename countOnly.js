0a99838820cb80a6d505fdeed60770b81564c6d1
yasu71 <yasuyo71@gmail.com>
2020-03-05 00:07:20 +0000

    w01d02 Implement countOnly - Add code to loop over all the items in the array and print them to the console

--- countOnly.js
+++ countOnly.js
@@ -11,6 +11,9 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {}
+    for (const item of allItems) {
+      console.log(item);
+    }
   return results;
 };
 
