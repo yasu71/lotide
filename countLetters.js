b9e79db78ae4058ccab8bdc32d9aabb34c65ff11
yasu71 <yasuyo71@gmail.com>
2020-03-05 01:10:55 +0000

    w01d01 Implement countLetters in Lotide - return an object where each unique character encountered in the string is a property of the object and the value for that property/key should be the number of occurrences for that character.

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log(`\uD83D\uDE00 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\uD83D\uDE08 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let countLetters = function(letters) {
+  let result = {}; 
+  letters = letters.split(' ');
+  letters = letters.join('');
+
+  for (let letter of letters){
+    //check if the current letter is in result obj
+    //add +1
+    if(result[letter]){
+      result[letter] += 1;
+    }     
+    //else add the key and value to 1
+    else {
+      result[letter] = 1;
+    }
+  }
+  return result
+};
+
+console.log(countLetters("lighthouse in the house"));
