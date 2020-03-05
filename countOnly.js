b685ca6e99f9d77589473c046435f210e03cb407
yasu71 <yasuyo71@gmail.com>
2020-03-05 00:08:18 +0000

    w01d02 Implement countOnly - Increment the count of each item into results as we encounter each string item in the array.

--- countOnly.js
+++ countOnly.js
@@ -12,7 +12,15 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   const results = {}
     for (const item of allItems) {
-      console.log(item);
+      // inside the loop, 
+      // increment the counter for each item:
+      //   set a property with that string key to:
+      //     the value that was already there (or zero if nothing there) with 1 added to it.
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   return results;
 };
