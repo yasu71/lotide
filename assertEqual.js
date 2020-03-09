355a91c97f695e1552809d04ffb754f22e12076d
yasu71 <yasuyo71@gmail.com>
2020-03-09 21:29:39 +0000

    added module.exports and test files by adding require

--- assertEqual.js
+++ assertEqual.js
@@ -1,20 +1,22 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected){
-    console.log("\uD83D\uDE00" + "Assertion Passed: " + actual + " === " + expected);
+  if (actual === expected) {
+    console.log(`\uD83D\uDE00 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("\uD83D\uDE08" + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`\uD83D\uDE08 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-// Comparing non-identical strings
-assertEqual("Lighthouse Labs", "Bootcamp");
+module.exports = assertEqual;
 
-// Comparing identical numbers
-assertEqual(1, 1);
+// // TEST CODE
+// // Comparing non-identical strings
+// assertEqual("Lighthouse Labs", "Bootcamp");
 
-// Comparing identical strings
-assertEqual("Yasu", "Yasu");
+// // Comparing identical numbers
+// assertEqual(1, 1);
 
-// Comparing non-identical numbers
-assertEqual(2, 1);
+// // Comparing identical strings
+// assertEqual("Hello", "Hello");
+
+// // Comparing non-identical numbers
+// assertEqual(2, 1);
